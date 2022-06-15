c3e455d284869750c3dcfe2e04cce01045224665
Nicola <Nicola@hd.com>
2022-06-15 17:13:21 +0800

    second commit

--- src/main/java/com/atguigu/Pojo/MessageResponse.java
+++ src/main/java/com/atguigu/Pojo/MessageResponse.java
@@ -3,5 +3,7 @@ package com.atguigu.Pojo;
 public class MessageResponse {
     public static void main(String[] args) {
         System.out.println("hello,git!");
+        System.out.println("hello,git!");
+        System.out.println("hello,git!");
     }
 }
